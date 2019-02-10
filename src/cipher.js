0122234dcf448ab3873ca640f6085f24693ce1df
jessikalayne <jessika.layne.pereira@gmail.com>
2019-02-10 18:19:31 -0200

    novas alterações na page i

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,7 @@
 function cipherCode() {
   const phrase = document.getElementById("frase").value;
   const number = parseInt(document.getElementById("deslocamento").value);
-  
-  const msgcrip = encode (phrase, number);
-
+  const msgcrip = encode(phrase, number);
   document.getElementById("mensagem").innerHTML = msgcrip;
 }
 
@@ -11,44 +9,35 @@ function mod(n, m) {
   return ((n % m) + m) % m;
 }
 
-
-function encode (phrase, offset) {
+function encode(phrase, offset) {
   let phraseCrip = "";
 
-
-  for (let i = 0; i <phrase.length; i++) {
+  for (let i = 0; i < phrase.length; i++) {
     let code = phrase.charCodeAt(i);
     if ((code >= 65) && (code <= 90)) {
       phraseUp = mod((code - 65 + offset), 26) + 65;
-      phraseCrip+= String.fromCharCode(phraseUp);
+      phraseCrip += String.fromCharCode(phraseUp);
     } else if ((code >= 97) && (code <= 122)) {
       phraseDown = mod((code - 97 + offset), 26) + 97;
       phraseCrip += String.fromCharCode(phraseDown);
     } else {
       phraseCrip += String.fromCharCode(code);
     }
-      
   }
-
   return phraseCrip;
-
 }
-
 
 function cipherDeCode() {
   const phrase = document.getElementById("frase2").value;
   const number = parseInt(document.getElementById("deslocamento2").value);
-  
-  const msgcrip = decode (phrase, number);
-
+  const msgcrip = decode(phrase, number);
   document.getElementById("mensagem2").innerHTML = msgcrip;
 }
 
-function decode (phrase, offset) {
+function decode(phrase, offset) {
   let phraseCrip = "";
 
-
-  for (let i = 0; i <phrase.length; i++) {
+  for (let i = 0; i < phrase.length; i++) {
     let code = phrase.charCodeAt(i);
     if ((code >= 65) && (code <= 90)) {
       phraseUp = mod((code - 65 - offset), 26) + 65;
@@ -59,10 +48,7 @@ function decode (phrase, offset) {
     } else {
       phraseCrip += String.fromCharCode(code);
     }
-      
   }
 
   return phraseCrip;
-
-
 }
